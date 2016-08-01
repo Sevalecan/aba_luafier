83c0cc4635b8ee13f349af3ca0469ea74cfdc911
Jonathan Seeley <sevalecan@gmail.com>
2016-08-01 04:28:45 -0400

    Added some new information to the code and added code for comparing some types between ba938 and advba 1.64

--- aba_luafier.py
+++ aba_luafier.py
@@ -185,6 +185,19 @@ aba_units = {}
 
 ba_units = {}
 
+# Usless tags mentioned here: https://github.com/ZeroK-RTS/SpringRTS-Tools/blob/41e0673f11ca5e90e354664cbab29fa7c40b609c/SpringModEdit/Procedures/removeUselessTags.lua
+# Some defaults listed in rts/Sim/Units/UnitDef.cpp for spring. URL: https://goo.gl/G4zUyE
+# This can help us get rid of variables that are sensibly defaulted in Spring.
+useless_tags = ["maneuverleashlength",
+	"bmcode",
+	"canreclamate",
+	"scale",
+	"steeringmode",
+	"tedclass",
+	"designation",
+"defaultmissiontype"]
+useless_set = set(useless_tags)
+
 
 for i in (aba_dir / 'features').rglob('*.tdf'):
 	feature = LoadTDF(str(i))
@@ -195,8 +208,12 @@ print("Features loaded: {0}".format(len(aba_features)))
 for i in (aba_dir / 'units').rglob('*.fbi'):
 	unit = LoadTDF(str(i))
 	unit = unit['UNITINFO']
+	unit_name = ""
 	FixUnitTypes(unit)
-	aba_units.update(unit)
+	for j,k in unit.items():
+		if j.lower() == "name":
+			unit_name = k
+	aba_units.update({unit_name: unit})
 
 print("Units loaded {0}".format(len(aba_units)))
 
@@ -211,7 +228,7 @@ aba_armor = LoadTDF(str((aba_dir / 'armor.txt')))
 print("Armors loaded: {0}".format(len(aba_armor)))
 
 for i in (ba_dir / 'units').glob('*.lua'):
-	print("Loading {0}".format(str(i.name)))
+	# print("Loading {0}".format(str(i.name)))
 	unit = LoadLUA(i)
 	ba_units.update(unit)
 
@@ -221,6 +238,42 @@ print("BA Units loaded {0}".format(len(ba_units)))
 print()
 if args.action == "ctypes":
 	print("Comparing variable types")
+	ba_vlist = set()
+	aba_vlist = set()
+	for i in ba_units.values():
+		for j,k in i.items():
+			if type(k) != type({}):
+				ba_vlist.add(j.lower())
+	for h,i in aba_units.items():
+		for j,k in i.items():
+			if type(k) != type({}):
+				aba_vlist.add(j.lower())
+				
+	ba_uniq = list(ba_vlist.difference(aba_vlist))
+	aba_uniq = list(aba_vlist.difference(ba_vlist))
+	
+	useless_aba = aba_vlist & useless_set
+	
+	col_width = max([len(word) for word in ba_uniq] + [len(word) for word in aba_uniq])+2
+
+	ba_uniq.sort()
+	aba_uniq.sort()
+	rows = max(len(ba_uniq), len(aba_uniq))
+	print("BA".ljust(col_width) + "ABA".ljust(col_width))
+	for i in range(0,rows):
+		ba_text = ""
+		aba_text = ""
+		try:
+			ba_text = ba_uniq[i]
+		except IndexError:
+			pass
+		try:
+			aba_text = aba_uniq[i]
+		except IndexError:
+			pass
+		print(ba_text.ljust(col_width) + aba_text.ljust(col_width))
+		
+	print(useless_aba)
 elif args.action == "lsubs":
 	subdir_list = set([])
 	print("Listing sub-dict names found in all lua units.")
@@ -230,10 +283,10 @@ elif args.action == "lsubs":
 	for h,i in ba_units.items():
 		for j,k in i.items():
 			if type(k) == type({}):
-				if j.lower() not in subdir_list:
-					print()
-					print("{0} in {1}:    {2}".format(j, h, k))
+				#if j.lower() not in subdir_list:
+					#print()
+					#print("{0} in {1}:    {2}".format(j, h, k))
 				subdir_list.add(j.lower())
-	print("Subdirs: {0}".format(subdir_list))
+	print("Subdicts in BA: {0}".format(subdir_list))
 				
 	
