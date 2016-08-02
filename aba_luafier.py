52beae0e9f5e677ea383e6929d54367f804b8a78
Jonathan Seeley <sevalecan@gmail.com>
2016-08-01 22:31:41 -0400

    Added comments to loaders.py and aba_luafier.py

--- aba_luafier.py
+++ aba_luafier.py
@@ -4,14 +4,14 @@ import re
 import lupa
 from lupa import LuaRuntime
 import argparse
-from loaders import LoadLUA,LoadTDF,FixUnitTypes,ExpandTable
+from loaders import LoadLUA,LoadTDF,FixUnitTypes,ExpandTable,LowerKeys
 
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("action", choices=["ctypes", "lsubs"], nargs='?')
-
+parser.add_argument("action", choices=["ctypes", "lsubs", "cweap"], nargs='?')
 args = parser.parse_args()
 
+# Set up the paths for our games to be analyzed.
 ba_dir = Path("../ba938")
 aba_dir = Path(".")
 
@@ -34,13 +34,17 @@ useless_tags = ["maneuverleashlength",
 "defaultmissiontype"]
 useless_set = set(useless_tags)
 
+# For weapons, badtargetcategory should get moved into the weapondef, as should weaponmaindir and onlytargetcategory
 
+# Load ABA featuredefs. Features usually correspond to units, and the luafied units have feature
+# information provided in the unitdef.
 for i in (aba_dir / 'features').rglob('*.tdf'):
 	feature = LoadTDF(str(i))
 	aba_features.update(feature)
 
 print("Features loaded: {0}".format(len(aba_features)))
 
+# Load ABA units. The TDF loader works because FBI files are essentially the same format.
 for i in (aba_dir / 'units').rglob('*.fbi'):
 	unit = LoadTDF(str(i))
 	unit = unit['UNITINFO']
@@ -53,16 +57,19 @@ for i in (aba_dir / 'units').rglob('*.fbi'):
 
 print("Units loaded {0}".format(len(aba_units)))
 
+# Load ABA weapondefs.
 for i in (aba_dir / 'weapons').rglob('*.tdf'):
 	weapon = LoadTDF(str(i))
 	aba_weapons.update(weapon)
 
+aba_weapons = LowerKeys(aba_weapons)
 print("Weapons loaded {0}".format(len(aba_weapons)))
 
+# Load ABA armor file.
 aba_armor = LoadTDF(str((aba_dir / 'armor.txt')))
-
 print("Armors loaded: {0}".format(len(aba_armor)))
 
+# Load BA938 Luafied units.
 for i in (ba_dir / 'units').glob('*.lua'):
 	# print("Loading {0}".format(str(i.name)))
 	unit = LoadLUA(i)
@@ -70,31 +77,51 @@ for i in (ba_dir / 'units').glob('*.lua'):
 
 print("BA Units loaded {0}".format(len(ba_units)))
 
+# Extract weapondefs from BA938 for comparison against ABA.
+ba_weapons = {}
+for key,value in ba_units.items():
+	if value.get("weapondefs", None) != None:
+		ba_weapons.update(value['weapondefs'])
+
+print("{0} BA Weapons found.".format(len(ba_weapons)))
+	
+
 # Deal with the action now that we've loaded data.
 print()
 if args.action == "ctypes":
 	print("Comparing variable types")
 	ba_vlist = set()
 	aba_vlist = set()
+	
+	# Get a list of variables from every unit in BA and ABA. Make sure these
+	# are lowercase.
 	for i in ba_units.values():
 		for j,k in i.items():
 			if type(k) != type({}):
 				ba_vlist.add(j.lower())
+				
 	for h,i in aba_units.items():
 		for j,k in i.items():
 			if type(k) != type({}):
 				aba_vlist.add(j.lower())
 				
+	# Get a list from the difference of each set. This tells us which
+	# variables each 'game' has that the other doesn't.
 	ba_uniq = list(ba_vlist.difference(aba_vlist))
 	aba_uniq = list(aba_vlist.difference(ba_vlist))
 	
+	# Are useless variables used in aba? This set will tell.
 	useless_aba = aba_vlist & useless_set
 	
+	# Use all values from the aba and ba unique variables and find the longest name in that set.
+	# Add 2 for the column width so that there's some clear spacing for each column.
 	col_width = max([len(word) for word in ba_uniq] + [len(word) for word in aba_uniq])+2
 
 	ba_uniq.sort()
 	aba_uniq.sort()
 	rows = max(len(ba_uniq), len(aba_uniq))
+	
+	# Print the columns for the unique variables in each mod.
 	print("BA".ljust(col_width) + "ABA".ljust(col_width))
 	for i in range(0,rows):
 		ba_text = ""
@@ -110,6 +137,7 @@ if args.action == "ctypes":
 		print(ba_text.ljust(col_width) + aba_text.ljust(col_width))
 		
 	print(useless_aba)
+	
 elif args.action == "lsubs":
 	subdir_list = set([])
 	print("Listing sub-dict names found in all lua units.")
@@ -124,5 +152,13 @@ elif args.action == "lsubs":
 					#print("{0} in {1}:    {2}".format(j, h, k))
 				subdir_list.add(j.lower())
 	print("Subdicts in BA: {0}".format(subdir_list))
-				
+	
+elif args.action == "cweap":
+	baw_set = set(ba_weapons.keys())
+	abaw_set = set(aba_weapons.keys())
+	
+	nset = baw_set & abaw_set
+	dset = abaw_set - nset
+	print(dset)
+	
 	
--- loaders.py
+++ loaders.py
@@ -5,20 +5,22 @@ import lupa
 from lupa import LuaRuntime
 import argparse
 
-
-
 # Converts a Lua table to nested dicts.
 def ExpandTable(table):
 	indices = []
 	lint = lupa.LuaRuntime()
-	tabletype = type(lint.eval('{a=[[b]]}'))
+	tabletype = type(lint.eval('{a=[[b]]}'))	# Get type info for a lupa table.
 	try:
+		# Convert the table to a dict if possible.
 		table = dict(table)
+		# Set up a list and continue through the tree converting
+		# lUA tables whenever encountered.
 		indices = [(table, x) for x in table.keys()]
 		
 		while len(indices) > 0:
 			tb,key = indices.pop()
 			try:
+				# Convert sub-variable to dict if possible.
 				if type(tb[key]) != tabletype:
 					continue
 				tb[key] = dict(tb[key])
@@ -26,7 +28,8 @@ def ExpandTable(table):
 			except (ValueError, TypeError):
 				pass
 	except (ValueError, TypeError):
-		pass
+		pass # If not possible to convert to a dict, assume it is a primitive Python type
+		pass # and needs no further manipulation.
 	return table
 
 def LoadLUA(filepath):
@@ -36,19 +39,26 @@ def LoadLUA(filepath):
 	
 	return ExpandTable(unit)
 
+# Custom exception for dropping out of more than one loop at a time.
 class ExitLoop( Exception ):
 		pass
 	
 def FixUnitTypes(unit):
+	# List the unit fields that should be a string type and make sure to convert them accordingly.
 	strfields = ["category", "buildpic", "buildinggrounddecaltype", "collisionvolumeoffsets", "collisionvolumescales", "collisionvolumetype", "corpse", "description", "explodeas", "icontype", "name", "objectname", "selfdestructas", "yardmap", "side", "name", "badtargetcategory", "copyright", "defaultmissiontype", "nochasecategory", "selfdestructas", "unitname", re.compile("w[a-z]{3}_badtargetcategory", re.I), re.compile("weapon[0-9]+", re.I), re.compile("weaponmaindir[0-9]+", re.I), re.compile("onlytargetcategory[0-9]*", re.I), re.compile("badtargetcategory[0-9]+", re.I), re.compile("explosiongenerator[0-9]+", re.I), "soundcategory", "designation", "tedclass", "movementclass", "tracktype", "flankingbonusdir", "script"]
+	# List of boolean fields for conversion. FBI Files used 1/0, lua uses true/false
 	boolfields = ["usebuildinggrounddecal", "builder", "canmove"]
 	loadfields = ["soundcategory"]
-	retype = type(re.compile(""))
+	retype = type(re.compile(""))	# Get type information for compiled regular expression.
 	
 	for i in unit.keys():
+		# lowercase keys make for easy comparison.
 		j = i.lower()
 		
 		try:
+			# Check if the variable is in strfields or boolfields and try to convert.
+			# Else try converting to int, or if that fails, float.
+			# If call conversions fail, do nothing???
 			for k in strfields:
 				if type(k) == retype:
 					if k.match(j):
@@ -69,14 +79,14 @@ def FixUnitTypes(unit):
 			try:
 				unit[i] = float(unit[i])
 			except (ValueError, TypeError):
-				pass
+				pass	# Reaching this point could indicate a problem with the FBI file format.
 		except ExitLoop:
 			pass
 	return unit
 
 	
 
-class TDFFrame:
+class TDFFrame:	# Class for holding frame information while parsing TDF files.
 	def __init__(self):
 		self.sections = {}
 		self.cursec = ""
@@ -92,15 +102,16 @@ class TDFFrame:
 # We can probably just use LoadTDF on all of them.		
 def LoadTDF(filename):
 	f = open(filename, 'rb')
-	tdf_data = f.read().decode("latin_1")
+	tdf_data = f.read().decode("latin_1")	# Encoding is latin_1. This is determined by a single unicode
+											# character present in one file.
 	#tdf_data = f.read().decode("UTF-8", "ignore")
 	newlines = "\r\n"
 	spaces = "\t "
-	frames = []
-	line = 1
-	fr = TDFFrame()
-	for i in tdf_data:
-		if i == '\n':
+	frames = []	# TDFFrames stack.
+	line = 1	# What line we're on for detecting a syntax error.
+	fr = TDFFrame()	# Initialize the current frame. Each sub-structure in a TDF gets its own parsing frame.
+	for i in tdf_data:	# Iterate over data loaded from the TDF or FBI file.
+		if i == '\n':	# Detect newline.
 			line = line + 1
 		if i == "/":	# Count slashes for comments.
 			fr.slashcount = fr.slashcount + 1
@@ -109,54 +120,53 @@ def LoadTDF(filename):
 				raise Exception("TDF Error: Expecting '{' in " + filename + ":" + str(line))
 			fr.slashcount = 0
 			
-		
-		if fr.state == 0:	# Waiting for a section header.
+		if fr.state == 0:	# Waiting for a section header. TODO: Check for comment in this state as well.
 			if i == '[': # Begin section header
 				fr.name = []
 				fr.state = 1
 		elif fr.state == 1: # Reading the header
 			if i == ']':
-				fr.cursec = ''.join(fr.name)
-				fr.sections[fr.cursec] = {}
-				fr.state = 2
+				fr.cursec = ''.join(fr.name)	# Get the name from parsed characters.
+				fr.sections[fr.cursec] = {}		# Clear the current section as we've
+				fr.state = 2					# just entered a new one.
 				continue
 			elif i not in newlines:
-				fr.name.append(i)
+				fr.name.append(i)				# load section name.
 			else:
 				raise Exception("TDF Error: newline or carriage return in section header.")
 		elif fr.state == 2:	# Waiting for a data section.
-			if fr.slashcount == 2:
-				fr.state = 4
-				fr.restate = 2
+			if fr.slashcount == 2:				# If we have a comment, skip until the next line.
+				fr.state = 4					# Set to comment state.
+				fr.restate = 2					# Remember the context the comment was placed in.
 			if i in newlines or i in spaces:
 				pass
-			elif i == "{":
+			elif i == "{":	# Entered data section. Initialize name list and change state.
 				fr.name = []
 				fr.state = 3
-			elif i != '/':
+			elif i != '/':	# Disallow anything other than the section block or a comment here.
 				raise Exception("TDF Error: Expecting '{' in " + filename + ":" + str(line))
-		elif fr.state == 3:	# Waiting for end of data section or some data.
-			if fr.slashcount == 2:
+		elif fr.state == 3:	# Waiting for end of data section or some more data.
+			if fr.slashcount == 2:	# Skip comment for this line, remember state.
 				fr.state = 4
 				fr.retstate = 3
-			elif i == "}":
-				if len(frames):
+			elif i == "}":	    # Data section ended.
+				if len(frames): # Drop to the next highest frame if there is one.
 					parent = frames[-1]
 					parent.sections[parent.cursec][fr.cursec] = fr.sections[fr.cursec]
 					fr = frames.pop()
 				else:
-					fr.state = 0
-			elif i in newlines:
+					fr.state = 0	# Else exit to state 0 and wait for EOF or another section header.
+			elif i in newlines:	# Skip
 				fr.name = []
-			elif i == "=":
+			elif i == "=":	# Variable name complete, next load the data in the variable.
 				fr.cname = "".join(fr.name)
 				fr.value = []
 				fr.state = 5
-			elif i == "[":
+			elif i == "[":	# A sub-section was encountered. Create a new TDF frame and push this one to the stack.
 				frames.append(fr)
 				fr = TDFFrame()
-				fr.state = 1
-			elif i not in spaces:
+				fr.state = 1	# In a new frame, but our section header has already started, so go to state 1.
+			elif i not in spaces:	# Append another name character to the list.
 				fr.name.append(i)
 		elif fr.state == 4: # We're in a comment. Wait for line end.
 			if i in newlines:
@@ -170,3 +180,16 @@ def LoadTDF(filename):
 				fr.value.append(i)
 	return fr.sections
 
+# Convert all dict keys and sub-dict keys to lowercase.
+def LowerKeys(data):
+	ndata = dict()
+	if type(data) == type(dict()):
+		for key,value in data.items():
+			if type(value) == type(dict):
+				value = LowerKeys(value)
+			if type(key) == type(""):
+				ndata[key.lower()] = value
+			else:
+				ndata[key] = value
+	return ndata
+			
